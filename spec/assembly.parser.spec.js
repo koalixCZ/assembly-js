f66263ce60bf272ac5747037e7235a98d8949b09
koalix <oskeruse@gmail.com>
2016-02-27 18:02:55 +0100

    code reformat

--- spec/assembly.parser.spec.js
+++ spec/assembly.parser.spec.js
@@ -1,16 +1,11 @@
 "use strict";
+
 var parser = require("../src/assembly.parser");
 
 describe("Parsing of HTML file", function () {
 	it("returns an array of sources from script elements", function (done) {
 		parser.parseHtmlAndReadScripts("spec/sample/index.html", function (scripts) {
-			expect(scripts).toEqual([
-				"1.js",
-				"2.js",
-				"3.js",
-				"4.js",
-				"5.js"
-			]);
+			expect(scripts).toEqual(["1.js", "2.js", "3.js", "4.js", "5.js"]);
 			done();
 		});
 	});
