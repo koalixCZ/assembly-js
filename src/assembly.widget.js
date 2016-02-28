2cb095c758a6b7d5086575d72051aa577c4f5a51
koalix <oskeruse@gmail.com>
2016-02-28 10:36:38 +0100

    widgets

---
+++ src/assembly.widget.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var config = require("./config.json");
+
+/**
+ * Returns name of the sourcer map file.
+ * @return {string}
+ */
+function getSourceMapName() {
+	return config.assembledJS + ".map";
+}
+
+
+module.exports.widget = {
+	getSourceMapName: getSourceMapName
+};
