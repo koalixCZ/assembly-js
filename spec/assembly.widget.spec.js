16fdc5da7ca5e96727aebb1f7e67a8615a382255
koalixCZ <oskeruse@gmail.com>
2016-04-26 10:16:33 +0200

    Now it is possible to assemble the file from an array of file names.

--- spec/assembly.widget.spec.js
+++ spec/assembly.widget.spec.js
@@ -5,6 +5,6 @@ var widget = require("../src/assembly.widget");
 describe("Widget", function () {
 
 	it("returns right source map name", function () {
-		expect(widget.getSourceMapName()).toBe("production.js.map");
+		expect(widget.getSourceMapName()).toBe("assembly.js.map");
 	});
 });
